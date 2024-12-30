8267dc2e01d399911a3deb2bc17010b24f0affda
Peter Okumbe <u18052640@tuks.co.za>
2024-12-30 17:36:13 +0200

    Renamed files and completed implementation of 2024 Day 2 (Part 1) challenge

--- 2024/D-0N3/python/24_day-1.py
+++ 2024/D-0N3/python/24_day-1.py
@@ -9,29 +9,29 @@
 # =====================================================================================================================
 import re
 
-lColumn = [] # int array var to store list of numbers in left column of text file
-rColumn = [] # int array var to store list of numbers in right column of text file
+arrLColumn = [] # int array var to store list of numbers in left column of text file
+arrRColumn = [] # int array var to store list of numbers in right column of text file
 
 # ! Open the file for reading mode (= default mode if the mode is not specified)
-file = open("../data/day-1-input.txt", "r")
+file = open("../data/24_day-1-input.txt", "r")
 
 # ! Read through EACH line in the text file
 for line in file:
     line = line.strip()
     # Split each line (by space separation) into two values
     num1, num2 = line.split()
-    lColumn.append(int(num1))  # Convert to integers and store in column1
-    rColumn.append(int(num2))  # Convert to integers and store in column2
+    arrLColumn.append(int(num1))  # Convert to integers and store in column1
+    arrRColumn.append(int(num2))  # Convert to integers and store in column2
 
 file.close()
 
 # Sort the arrays
-lColumn.sort()
-rColumn.sort()
+arrLColumn.sort()
+arrRColumn.sort()
 
 # Output the sorted arrays
-print("Column 1 (L):", lColumn)
-print("Column 2 (R):", rColumn)
+print("Column 1 (L):", arrLColumn)
+print("Column 2 (R):", arrRColumn)
 
 
 # %% [markdown]
@@ -47,27 +47,27 @@ print("Column 2 (R):", rColumn)
 #   - Then add up all the distances between EACH pair to get the *total distance*.
 # ---------------------------------------------------------------------------------------------------------------------
 totalDistance = 0 # int var to store the total distance between all pairings
-pairDistances = [] # int array var to store the distances between EACH pairing of numbers between the 2 columns
+arrPairDistances = [] # int array var to store the distances between EACH pairing of numbers between the 2 columns
 counter = 0
 
 # Calculate distance between each pairings
-for lNum in lColumn:
+for lNum in arrLColumn:
     distance = 0
-    rNum = rColumn[counter]
+    rNum = arrRColumn[counter]
 
     if lNum < rNum:
         distance = rNum - lNum
     else:
         distance = lNum - rNum
     
-    pairDistances.append(distance)
+    arrPairDistances.append(distance)
 
     counter += 1
 
 # Calculate total distance between all pairings
-totalDistance = sum(pairDistances)
+totalDistance = sum(arrPairDistances)
 
-print(pairDistances)
+print(arrPairDistances)
 print("Total distance:", totalDistance)
 
 # ====================================================================================================================
@@ -78,26 +78,26 @@ print("Total distance:", totalDistance)
 # %%
 # *** [PART 2] ***
 # ! PROBLEM: This time, you'll need to figure out exactly *how often* EACH number from the LEFT list appears in the RIGHT list.
-# - Calculate a *total similarity score* by adding up EACH number in the LEFT list after multiplying EACH number by the *number of times* that it appears in the RIGHT list.
+# - TASK: Calculate a *total similarity score* by adding up EACH number in the LEFT list after multiplying EACH number by the *number of times* that it appears in the RIGHT list.
 # ---------------------------------------------------------------------------------------------------------------------
 from collections import Counter
 
 totalSimilarityScore = 0
-lColumnSimilarityScores = [] # int array var to store similarity scores of EACH number in the LEFT column
+arrLColumnSimilarityScores = [] # int array var to store similarity scores of EACH number in the LEFT column
 
 # ! Calculate similarity scores of EACH number in the LEFT column
 # Count the number of times that EACH number in the RIGHT column appears in the same RIGHT column
-counter = Counter(rColumn)
+counter = Counter(arrRColumn)
 
-for num in lColumn:
+for num in arrLColumn:
     # func 'counter.get(x)' returns the number of occurrences of the number 'x'; 0 = default return value
     similarityScore = num * counter.get(num, 0)
     #print(num, '\t', similarityScore)
-    lColumnSimilarityScores.append(similarityScore)
+    arrLColumnSimilarityScores.append(similarityScore)
 
-totalSimilarityScore = sum(lColumnSimilarityScores)
+totalSimilarityScore = sum(arrLColumnSimilarityScores)
 
-print(lColumnSimilarityScores)
+print(arrLColumnSimilarityScores)
 print(totalSimilarityScore)
 
 
